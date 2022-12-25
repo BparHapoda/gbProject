4be0dd0c03ce308d0f1eec4de9e90f7f847af01f
Bpar_Hapoda <cddvdrom@yandex.ru>
2022-12-26 02:13:50 +0300

    add input strings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите 5 строк : ");
+        String input;
+        String [] date=new String[5];
+        int count=date.length;
+        while (count>0){
+            input=scanner.nextLine();
+            date[count]=input;
+
+        }
     }
 }
